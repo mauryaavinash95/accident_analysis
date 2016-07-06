ccf29cd1ffac5f313b4640d57999c283de2179c2
pratiklodha95 <lodhapratik9@gmail.com>
2016-07-06 11:44:48 +0530

    csv importer

--- website/website/views.py
+++ website/website/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-
+from django.contrib.admin.views.decorators import staff_member_required
 
 #PUTTING UP LOGIN PART AND REGISTRATION PART
 
@@ -9,3 +9,19 @@ def index(request):
     html = "<center><h1>ROAD ACCIDENT RECORDING FORM A1</h1></center><br>"
     html += "<a href='forms'>CONTINUE TO FILL FORM</a>"
     return HttpResponse(html)
+
+@staff_member_required
+def my_admin_view(request):
+    if request.method == "POST":
+        form = DataInput(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            success = True
+            context = {"form": form, "success": success}
+            return render_to_response("imported.html", context,
+            context_instance=RequestContext(request))
+    else:
+        form = DataInput()        
+        context = {"form": form}
+        return render_to_response("imported.html", context,
+        context_instance=RequestContext(request)) 
